8a3ce2724a50d7fbf2c5e4dd45a037eda4a9718d
Victor Milovanov <lost@losttech.software>
2019-03-20 20:07:10 -0700

    added tests for powers of two

---
+++ tests/PowersOfTwoTests.cs
@@ -0,0 +1,19 @@
+﻿namespace TypeNum.Tests {
+    using System;
+    using Xunit;
+    public class PowersOfTwoTests {
+        [Fact]
+        public void NumsAreCorrect() {
+            int num = default(N4096<int>).Num;
+
+            Assert.Equal(expected: 4096, num);
+        }
+
+        [Fact]
+        public unsafe void SizesAreCorrect() {
+            int size = sizeof(N4096<byte>);
+
+            Assert.Equal(expected: 4096, size);
+        }
+    }
+}
