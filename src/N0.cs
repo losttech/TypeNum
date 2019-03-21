a346f0b05ea14047653403b63830fe27f5a5ad10
Victor Milovanov <lost@losttech.software>
2019-03-20 20:02:33 -0700

    added N0

---
+++ src/N0.cs
@@ -0,0 +1,11 @@
+﻿namespace TypeNum {
+    using System.Runtime.CompilerServices;
+    using System.Runtime.InteropServices;
+    [StructLayout(LayoutKind.Sequential)]
+    public struct N0<T> : Numeral<T> where T : unmanaged {
+        public int Num {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => 0;
+        }
+    }
+}
