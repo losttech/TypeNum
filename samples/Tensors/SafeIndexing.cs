1350c28b5c4f959c82c00e0e6e66dd0cd2fd5f3f
Victor Milovanov <lost@losttech.software>
2020-02-06 01:07:38 -0800

    safe indexing sample

---
+++ samples/Tensors/SafeIndexing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static TypeNum.FixedArray;
+
+namespace TypeNum {
+    class SafeIndexing<Shape, T> where Shape: unmanaged, Numeral<int> {
+        object Get(object key) => throw new NotImplementedException();
+        void Set(object key, object value) => throw new NotImplementedException();
+
+        T this[FixedArray<Shape, int> indexes] {
+            get => (T)this.Get(indexes);
+            set => this.Set(indexes, value);
+        }
+
+        static void Sample() {
+            var a = new SafeIndexing<N2<int>, float>();
+            a[A(1, 2)] = 1.5f;
+        }
+    }
+}
